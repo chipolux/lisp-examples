ec0f730a9b03801e2ca4232a785b558d26c20178
NaKyle Wright <nakyle.wright@gmail.com>
2017-07-27 12:53:10 -0500

    Let's stick to calling the header section of an email header.

--- email/main.lisp
+++ email/main.lisp
@@ -5,6 +5,6 @@
 (defun collect-plain-emails (dir)
   (loop
     for path in (pathing:list-directory dir)
-    for (headers body) = (multiple-value-list (email:read-email-file path))
-    when (email:plaintext-p headers)
+    for (header body) = (multiple-value-list (email:read-email-file path))
+    when (email:plaintext-p header)
     collect body))
