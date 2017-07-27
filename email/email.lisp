516bac907f8a1eb346f032a961aa3e2554815145
NaKyle Wright <nakyle.wright@gmail.com>
2017-07-27 12:52:14 -0500

    Add basic functionality to read-email-file, stub out plaintext-p

--- email/email.lisp
+++ email/email.lisp
@@ -1,5 +1,24 @@
 (defpackage :email
   (:use :common-lisp)
   (:export
-    :read-mail-file
+    :read-email-file
     :plaintext-p))
+
+(in-package :email)
+
+
+;;; It is safer to use iso-8859-1 when reading email files than the default
+#+CLISP
+(defconstant +encoding+ charset:iso-8859-1)
+#+(or CCL SBCL)
+(defconstant +encoding+ :iso-8859-1)
+
+
+(defun read-email-file (path)
+    (with-open-file (f path :direction :input :external-format +encoding+)
+      (values
+        (loop for line = (read-line f) until (equal line "") collect line)
+        (loop for line = (read-line f nil) while line collect line))))
+
+
+(defun plaintext-p (header) t)
