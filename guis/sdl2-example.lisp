bce2cf317f6ee031cb9fb99a4c1b296d60858456
NaKyle Wright <nakyle.wright@gmail.com>
2017-08-21 23:38:53 -0500

    Update sdl2 example to always flush output.

--- guis/sdl2-example.lisp
+++ guis/sdl2-example.lisp
@@ -1,14 +1,18 @@
 (require :sdl2)
 (require :cl-opengl)
 
+(defun format-now (msg &rest args)
+  "Output and flush MSG to STDOUT with arguments ARGS"
+  (apply #'format t msg args)
+  (finish-output))
+
 (defun basic-test ()
   "The kitchen sink."
   (sdl2:with-init (:everything)
-    (format t "Using SDL Library Version: ~D.~D.~D~%"
+    (format-now "Using SDL Library Version: ~D.~D.~D~%"
             sdl2-ffi:+sdl-major-version+
             sdl2-ffi:+sdl-minor-version+
             sdl2-ffi:+sdl-patchlevel+)
-    (finish-output)
 
     (sdl2:with-window (win :flags '(:shown :opengl))
       (sdl2:with-gl-context (gl-context win)
@@ -16,8 +20,7 @@
               (haptic ()))
 
           ;; basic window/gl setup
-          (format t "Setting up window/gl.~%")
-          (finish-output)
+          (format-now "Setting up window/gl.~%")
           (sdl2:gl-make-current win gl-context)
           (gl:viewport 0 0 800 600)
           (gl:matrix-mode :projection)
@@ -27,12 +30,11 @@
           (gl:clear-color 0.0 0.0 1.0 1.0)
           (gl:clear :color-buffer)
 
-          (format t "Opening game controllers.~%")
-          (finish-output)
+          (format-now "Opening game controllers.~%")
           ;; open any game controllers
           (loop for i from 0 upto (- (sdl2:joystick-count) 1)
              do (when (sdl2:game-controller-p i)
-                  (format t "Found gamecontroller: ~a~%"
+                  (format-now "Found gamecontroller: ~a~%"
                           (sdl2:game-controller-name-for-index i))
                   (let* ((gc (sdl2:game-controller-open i))
                          (joy (sdl2:game-controller-get-joystick gc)))
@@ -43,18 +45,17 @@
                         (sdl2:rumble-init h))))))
 
           ;; main loop
-          (format t "Beginning main loop.~%")
-          (finish-output)
+          (format-now "Beginning main loop.~%")
           (sdl2:with-event-loop (:method :poll)
             (:keydown (:keysym keysym)
              (let ((scancode (sdl2:scancode-value keysym))
                    (sym (sdl2:sym-value keysym))
                    (mod-value (sdl2:mod-value keysym)))
                (cond
-                 ((sdl2:scancode= scancode :scancode-w) (format t "~a~%" "WALK"))
+                 ((sdl2:scancode= scancode :scancode-w) (format-now "~a~%" "WALK"))
                  ((sdl2:scancode= scancode :scancode-s) (sdl2:show-cursor))
                  ((sdl2:scancode= scancode :scancode-h) (sdl2:hide-cursor)))
-               (format t "Key sym: ~a, code: ~a, mod: ~a~%"
+               (format-now "Key sym: ~a, code: ~a, mod: ~a~%"
                        sym
                        scancode
                        mod-value)))
@@ -64,11 +65,11 @@
                (sdl2:push-event :quit)))
 
             (:mousemotion (:x x :y y :xrel xrel :yrel yrel :state state)
-             (format t "Mouse motion abs(rel): ~a (~a), ~a (~a)~%Mouse state: ~a~%"
+             (format-now "Mouse motion abs(rel): ~a (~a), ~a (~a)~%Mouse state: ~a~%"
                      x xrel y yrel state))
 
             (:controlleraxismotion (:which controller-id :axis axis-id :value value)
-             (format t "Controller axis motion: Controller: ~a, Axis: ~a, Value: ~a~%"
+             (format-now "Controller axis motion: Controller: ~a, Axis: ~a, Value: ~a~%"
                      controller-id axis-id value))
 
             (:controllerbuttondown (:which controller-id)
@@ -89,8 +90,7 @@
 
             (:quit () t))
 
-          (format t "Closing opened game controllers.~%")
-          (finish-output)
+          (format-now "Closing opened game controllers.~%")
           ;; close any game controllers that were opened
           ;; as well as any haptics
           (loop for (i . controller) in controllers
