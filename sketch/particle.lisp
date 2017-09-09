59c9ecd098adda3cd1feaeb4f184be3cf7edbb71
NaKyle Wright <nakyle.wright@gmail.com>
2017-09-08 19:30:52 -0500

    Add acceleration and gravity handling to particle.

--- sketch/particle.lisp
+++ sketch/particle.lisp
@@ -1,16 +1,24 @@
 (defpackage :particle
   (:use :cl :vector)
-  (:export :particle :update))
+  (:export :particle :update :accelerate))
 (in-package :particle)
 
 
 (defclass particle ()
   ((position)
-   (velocity)))
+   (velocity)
+   (gravity)))
 
-(defmethod initialize-instance :after ((p particle) &key x y speed direction)
+(defmethod initialize-instance :after ((p particle) &key x y speed direction gravity)
   (setf (slot-value p 'position) (make-instance 'vector2 :x x :y y))
-  (setf (slot-value p 'velocity) (make-instance 'vector2 :length speed :angle direction)))
+  (setf (slot-value p 'velocity) (make-instance 'vector2 :length speed :angle direction))
+  (if gravity
+    (setf (slot-value p 'gravity) (make-instance 'vector2 :x 0 :y gravity))
+    (setf (slot-value p 'gravity) (make-instance 'vector2 :x 0 :y 0))))
 
 (defmethod update ((p particle))
+  (add! (slot-value p 'position) (slot-value p 'gravity))
   (add! (slot-value p 'position) (slot-value p 'velocity)))
+
+(defmethod accelerate ((p particle) accel)
+  (add! (slot-value p 'velocity) accel))
