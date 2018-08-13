b041a6ac9a993770cbc9225e2da069927035b092
NaKyle Wright <nakyle.wright@gmail.com>
2018-08-13 11:29:46 -0500

    Tweak quicklisp install script.

--- tools/install-quicklisp.lisp
+++ tools/install-quicklisp.lisp
@@ -1,6 +1,9 @@
 ;;; if ~/.quicklisp doesn't exist, install quicklisp there
+(load "quicklisp.lisp")
+
 (unless (probe-file "~/.quicklisp")
-  (load "quicklisp.lisp")
+  (format t "Installing quicklisp...")
   (quicklisp-quickstart:install :path "~/.quicklisp"))
 
+(format t "Adding quicklisp to init file...")
 (ql:add-to-init-file)
